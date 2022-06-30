cab85a2995eebbc16b47405d343934a57b759504
Matthew Crick <Matthewcrick@outlook.com>
2022-06-30 18:22:39 +1000

    merged receipt page to menu

--- digital-receipt/src/App.js
+++ digital-receipt/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './home';
+import BasicGrid from './container';
 import ListPage from './list';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import HomePage from './home';
 
 function App() {
   return (
@@ -13,9 +14,13 @@ function App() {
         <Navbar className = "navBar">
         <Link to = "/home" className = "link">Home</Link>
         <Link to = "/list" className = "link">Receipts</Link>
+        <Link to = "/receipt" className = "link">Receipt</Link>
         </Navbar>
         <hr></hr>
         <Switch>
+        <Route exact path = "/receipt">
+           <BasicGrid />
+          </Route>
           <Route exact path = "/list">
            <ListPage />
           </Route>
